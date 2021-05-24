f5993e44fa56b50dff7c079e50c21f6f92e6be6e
Chris Rohan Yeeda <66123219+RohanYeeda@users.noreply.github.com>
2021-05-24 13:45:51 +0530

    Add files via upload

---
+++ TableUsingAntD/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Table from './components/table'
+import './components/table.css'
+function App() {
+  return (
+    <div className="container">
+    
+        <Table/>
+      
+    </div>
+  );
+}
+
+export default App;
